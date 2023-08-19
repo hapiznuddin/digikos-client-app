1000e316b10a479dd351d1c93d0386177e398e4f
Hapiz Nuddin <hapiznuddin@gmail.com>
2023-08-19 17:25:43 +0700

    feat: first setup project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='bg-slate-400'>
+      <h1 className='text-3xl font bold'>Vite + React</h1>
+    </div>
+  )
+}
+
+export default App
