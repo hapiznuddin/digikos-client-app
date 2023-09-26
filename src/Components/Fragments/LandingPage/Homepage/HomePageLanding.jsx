cac4dd3fb33ff2afbb9e0409a942ccbeb5212781
hapiznuddin <hapiznuddin@gmail.com>
2023-09-26 23:07:10 +0700

    UI(landingPage): membuat facility section

--- src/Components/Fragments/LandingPage/Homepage/HomePageLanding.jsx
+++ src/Components/Fragments/LandingPage/Homepage/HomePageLanding.jsx
@@ -1,17 +1,20 @@
 import LandingPageLayout from "../../../Layouts/LandingPageLayout";
 import HeroSection from "./HeroSection";
+import FacilitySection from "./FacilitySection";
 import "./HomePage.css";
 
 const HomePageLanding = () => {
   return (
     <LandingPageLayout>
-    <div className="flex flex-col justify-center items-center">
-      <div className="bgHome  pb-6 h-full md:h-[500px] lg:h-[935px] rounded-[24px] md:rounded-[48px] m-2 -mt-[70px] md:m-4 md:-mt-24 lg:-mt-24">
-        
-        <HeroSection />
+      <div className="flex flex-col justify-center items-center">
+        <div className="bgHome  pb-6 h-full md:h-[500px] lg:h-[935px] rounded-[24px] md:rounded-[48px] m-2 -mt-[70px] md:m-4 md:-mt-24 lg:-mt-24">
+          <HeroSection />
+        </div>
+        <FacilitySection />
+        <div className="mt-20">
+          <img src="/bg-facility.webp" />
+        </div>
       </div>
-        
-    </div>
     </LandingPageLayout>
   );
 };
--- src/pages/landingPage/homePage.jsx
+++ src/pages/landingPage/homePage.jsx
@@ -1,13 +1,43 @@
 import {HelmetProvider } from "react-helmet-async";
 import HomePageLanding from "../../Components/Fragments/LandingPage/Homepage/HomePageLanding";
 import HeadMetaData from "../../Components/HeadMetaData";
+import { useEffect, useState } from "react";
+import ButtonPrimary from "../../Components/Elements/Button";
+import { HiChevronDoubleUp } from "react-icons/hi";
 
 const HomePage = () => {
+  const [onTop, setOnTop] = useState(false);
+  useEffect(() => {
+    // Fungsi untuk memeriksa posisi scroll
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setOnTop(true);
+      } else {
+        setOnTop(false);
+      }
+    };
+    // Menambahkan event listener saat komponen dimuat
+    window.addEventListener('scroll', handleScroll);
+    // Membersihkan event listener saat komponen di-unmount
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo(0, 0);
+  };
+
   return (
     <HelmetProvider>
         <HeadMetaData title="Beranda"/>
       <div>
         <HomePageLanding />
+      </div>
+      <div className="fixed bottom-4 right-4">
+        <ButtonPrimary className='px-2 shadow-2xl' onClick={scrollToTop} disabled={onTop}>
+          <HiChevronDoubleUp size={32}/>
+        </ButtonPrimary>
       </div>
     </HelmetProvider>
   );
