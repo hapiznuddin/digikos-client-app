8e5d38c9b7d4acf872ef76f64a76d5225654cd6c
hapiznuddin <hapiznuddin@gmail.com>
2023-09-30 14:10:07 +0700

    fix(navbar): navbar user login

--- src/Components/Layouts/LandingPageLayout/Navbar.jsx
+++ src/Components/Layouts/LandingPageLayout/Navbar.jsx
@@ -4,6 +4,8 @@ import { useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
 import Cookies from "js-cookie";
+import { AiOutlineUser } from "react-icons/ai";
+import { RxDashboard } from "react-icons/rx";
 
 // const token = Cookies.get("token");
 const role = Cookies.get("role");
@@ -87,21 +89,37 @@ const Navbar = () => {
       <div className="flex justify-center items-center w-64 ">
         {isLogin ? (
           <div className="dropdown dropdown-end">
-          <div className="hidden md:flex gap-2 justify-center items-center w-full px-2 ">
-            <button className="btn btn-ghost  m-1 rounded-full text-base font-medium normal-case  hover:bg-primary-50/50" tabIndex={0}>
-            <div className="avatar">
-              <div className="w-9 rounded-full">
-                <img src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cGVvcGxlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" />
-              </div>
+            <div className="hidden md:flex gap-2 justify-center items-center w-full px-2 ">
+              <button
+                className="btn btn-ghost  m-1 rounded-full text-base font-medium normal-case  hover:bg-primary-50/50"
+                tabIndex={0}
+              >
+                <div className="avatar">
+                  <div className="w-9 rounded-full">
+                    <img src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cGVvcGxlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" />
+                  </div>
+                </div>
+                <p className="text-base font-medium">Nama User</p>
+              </button>
             </div>
-            <p className="text-base font-medium">Nama User</p>
-            </button>
+            <ul
+              tabIndex={0}
+              className="dropdown-content z-[1] menu p-2 gap-2 shadow-md bg-neutral-25 rounded-box w-64 flex flex-col justify-between"
+            >
+              <li className="text-base">
+                <a>
+                  <AiOutlineUser size={24} />
+                  <p>Profil</p>
+                </a>
+              </li>
+              <li className="text-base">
+                <a>
+                  <RxDashboard size={24} />
+                  <p>Dashboard</p>
+                </a>
+              </li>
+            </ul>
           </div>
-          <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
-            <li><a>Item 1</a></li>
-            <li><a>Item 2</a></li>
-          </ul>
-        </div>
         ) : (
           <div className=" gap-2 hidden md:flex bg-primary-600">
             <ButtonPrimary
@@ -147,20 +165,53 @@ const Navbar = () => {
               </a>
             </li>
             <div className="divider w-full -my-1"></div>
-            <div className="flex justify-between gap-1 w-full mt-4">
-              <ButtonPrimary
-                onClick={() => navigate("/login")}
-                className="w-[110px] text-base btn- bg-primary-50 text-primary-500 shadow-none hover:bg-primary-100 active:bg-primary-100"
-              >
-                Login
-              </ButtonPrimary>
-              <ButtonPrimary
-                onClick={() => navigate("/register")}
-                className="w-[110px] text-base btn- shadow-none"
-              >
-                Daftar
-              </ButtonPrimary>
-            </div>
+            {isLogin ? (
+              <ul className="menu w-full rounded-box flex">
+                <li>
+                  <details open>
+                    <summary className="rounded-full">
+                      <div className=" m-1 rounded-full flex gap-2 justify-start items-center text-base font-medium normal-case w-full">
+                        <div className="avatar">
+                          <div className="w-8 rounded-full">
+                            <img src="https:images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cGVvcGxlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" />
+                          </div>
+                        </div>
+                        <p className="text-base font-medium">Nama User</p>
+                      </div>
+                    </summary>
+                    <ul>
+                      <li className="text-base">
+                        <a>
+                          <AiOutlineUser size={24} />
+                          <p>Profil</p>
+                        </a>
+                      </li>
+                      <li className="text-base">
+                        <a>
+                          <RxDashboard size={24} />
+                          <p>Dashboard</p>
+                        </a>
+                      </li>
+                    </ul>
+                  </details>
+                </li>
+              </ul>
+            ) : (
+              <div className="flex justify-between gap-1 w-full mt-4">
+                <ButtonPrimary
+                  onClick={() => navigate("/login")}
+                  className="w-[110px] text-base btn- bg-primary-50 text-primary-500 shadow-none hover:bg-primary-100 active:bg-primary-100"
+                >
+                  Login
+                </ButtonPrimary>
+                <ButtonPrimary
+                  onClick={() => navigate("/register")}
+                  className="w-[110px] text-base btn- shadow-none"
+                >
+                  Daftar
+                </ButtonPrimary>
+              </div>
+            )}
           </ul>
         </div>
       </div>
