d961a10b716f591a061281e8d5bfe26a752cdba6
Hapiz Nuddin Setiadi <55013162+hapiznuddin@users.noreply.github.com>
2025-09-20 17:42:13 +0700

    feat : dockerfile

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: "0.0.0.0",
+    port: 3000
+  },
+  preview: {
+    port: 3001
+  }
 })
